20de2a50b9bd31cb92c205d06f3839f9382efd0e
MidnightOwll <dv92040@gmail.com>
2021-02-02 16:56:29 +0300

    website development

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html >
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link  rel="stylesheet" href="css/style.css">
+    <link rels="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/site.js"></script>
+    <title>YourShop</title>
+</head>
+<body>
+    <header>
+       <div id="hederInside">
+           <div id="logo"></div>
+           <div id="companyName">YourShop</div>
+           <div id="navWrap">
+               <a href="#">Главная</a>
+               <a href="#">Магазин</a>
+           </div>
+       </div>
+    </header>
+   
+    <div id="content">
+
+       <     <div id="promo">
+            <h1 id="promoText">
+                Здесь будет рекламный заголовок
+            </h1>
+        </div>
+    
+        <div id="mainTextWrap">
+            <div id="mainText">
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            </div>
+        </div>
+    
+        <div style="width: 1100px; margin: 0 auto">
+            <hr>
+            <h2>
+                Это будет отдельная страница с открытым магзином
+            </h2>
+    
+            <div style="width: 1100px; margin: 0 auto">
+                <h1>
+                    Каталог товаров
+                </h1>
+                <div>
+                    <div class="shopUnit">
+                        <img src="http://placehold.it/308x231" />
+    
+                        <div class="shopUnitName">
+                            Название продукта
+                        </div>
+                        <div class="shopUnitShortDesc">
+                            Здесь будет немного текста описывающего продукт.
+                            Здесь будет немного текста описывающего продукт.
+                            Здесь будет немного текста описывающего продукт.
+                        </div>
+                        <div class="shopUnitPrice">
+                            Цена: 0000 $
+                        </div>
+                        <a href="#" class="shopUnitMore">
+                            Подробнее
+                        </a>
+                    </div>
+    
+                    <div class="shopUnit">
+                        <img src="http://placehold.it/308x231" />
+    
+                        <div class="shopUnitName">
+                            Название продукта
+                        </div>
+                        <div class="shopUnitShortDesc">
+                            Здесь будет немного текста описывающего продукт.
+                            Здесь будет немного текста описывающего продукт.
+                            Здесь будет немного текста описывающего продукт.
+                        </div>
+                        <div class="shopUnitPrice">
+                            Цена: 0000 $
+                        </div>
+                        <a href="#" class="shopUnitMore">
+                            Подробнее
+                        </a>
+                    </div>
+    
+                    <div class="shopUnit">
+                        <img src="http://placehold.it/308x231" />
+    
+                        <div class="shopUnitName">
+                            Название продукта
+                        </div>
+                        <div class="shopUnitShortDesc">
+                            Здесь будет немного текста описывающего продукт.
+                            Здесь будет немного текста описывающего продукт.
+                            Здесь будет немного текста описывающего продукт.
+                        </div>
+                        <div class="shopUnitPrice">
+                            Цена: 0000 $
+                        </div>
+                        <a href="#" class="shopUnitMore">
+                            Подробнее
+                        </a>
+                    </div>
+                </div>
+            </div>
+    
+            <div style="margin-bottom: 20px; float: left;">
+                <hr>
+                <h2>
+                    Это будет отдельная страница с открытым товаром
+                </h2>
+    
+                <div id="openedProduct-img">
+                    <img src="http://placehold.it/342x260">
+                </div>
+                <div id="openedProduct-content">
+                    <h1 id="openedProduct-name">
+                        Название продукта
+                    </h1>
+                    <div id="openedProduct-desc">
+                        Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
+                        Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
+                        Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
+                    </div>
+                    <div id="openedProduct-price">
+                        Цена: 0000 $
+                    </div>
+                </div>
+            </div>
+    
+    
+        </div>
+        
+    </div>
+
+    <footer>
+        <div id="footerInside">
+    
+            <div id="contacts">
+                <div class="contactWrap">
+                    <img src="img/icon-email.svg" class="contactIcon">
+                    info@YourShop.ru
+                </div>
+                <div class="contactWrap">
+                    <img src="img/phone-call.svg" class="contactIcon">
+                    8 800 *** ** **
+                </div>
+                <div class="contactWrap">
+                    <img src="img/placeholder.svg" class="contactIcon">
+                   Краснодар, ул. ??????, д. * офис ?????
+                </div>
+            </div>
+        
+            <div id="footerNav">
+                <a href="#">Главная</a>
+                <a href="#">Магазин</a>
+            </div>
+    
+            <div id="social">
+                <img class="socialItem" src="img/vk-social-logotype.svg">
+                <img class="socialItem" src="img/google-plus-social-logotype.svg">
+                <img class="socialItem" src="img/facebook-logo.svg">
+            </div>
+    
+            <div id="copyrights">&copy; YourShop</div>
+        </div>
+    </footer>
+</body>
+
+</html>
+
+
