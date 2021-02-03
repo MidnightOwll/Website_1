802bbd0aeecec809f74f17805446e62f7d2c736b
MidnightOwll <dv92040@gmail.com>
2021-02-03 18:47:32 +0300

    rework of the site

--- index.php
+++ index.php
@@ -4,178 +4,92 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link  rel="stylesheet" href="css/style.css">
-    <link rels="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link  href="css/site.css" rel="stylesheet">
     <script src="js/jquery.js"></script>
     <script src="js/site.js"></script>
     <title>YourShop</title>
 </head>
-<body>
-    <header>
-       <div id="hederInside">
-           <div id="logo"></div>
-           <div id="companyName">YourShop</div>
-           <div id="navWrap">
-               <a href="#">Главная</a>
-               <a href="#">Магазин</a>
-           </div>
-       </div>
-    </header>
-   
-    <div id="content">
+    <body>
+        <header>
+        <div id="hederInside">
+            <div id="logo"></div>
+            <div id="companyName">YourShop</div>
+            <div id="navWrap">
+                <a href="/">Главная</a>
+                <a href="index.php?page=shop">Магазин</a>
+            </div>
+        </div>
+        </header>
+    
+        <div id="content">
+            <?php 
+            $goods=[
+                [
+                    'id'=> 1,
+                    'name' => 'Asus Zenbook UX331FA-EG007T 13.3',
+                    'img' => '/img/goods/Asus_Zenbook_UX331FA-EG007T_13.3.jpg',
+                    'desc' => 'Описание товара',
+                    'price' => '0000 $'
+                ],
+                [
+                    'id'=> 2,
+                    'name' => 'MacBook',
+                    'img' => '/img/goods/MacBook.jpg',
+                    'desc' => 'Описание товара',
+                    'price' => '0000 $'
+                ],
+                [
+                    'id'=> 3,
+                    'name' => 'Microsoft Surface Laptop 3',
+                    'img' => '/img/goods/Microsoft_Surface_Laptop_3.jpg',
+                    'desc' => 'Описание товара',
+                    'price' => '0000 $'
+                ]
+                ];
+            $page= $_GET['page'];
 
-       <     <div id="promo">
-            <h1 id="promoText">
-                Здесь будет рекламный заголовок
-            </h1>
-        </div>
-    
-        <div id="mainTextWrap">
-            <div id="mainText">
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-                Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            </div>
-        </div>
-    
-        <div style="width: 1100px; margin: 0 auto">
-            <hr>
-            <h2>
-                Это будет отдельная страница с открытым магзином
-            </h2>
-    
-            <div style="width: 1100px; margin: 0 auto">
-                <h1>
-                    Каталог товаров
-                </h1>
-                <div>
-                    <div class="shopUnit">
-                        <img src="http://placehold.it/308x231" />
-    
-                        <div class="shopUnitName">
-                            Название продукта
-                        </div>
-                        <div class="shopUnitShortDesc">
-                            Здесь будет немного текста описывающего продукт.
-                            Здесь будет немного текста описывающего продукт.
-                            Здесь будет немного текста описывающего продукт.
-                        </div>
-                        <div class="shopUnitPrice">
-                            Цена: 0000 $
-                        </div>
-                        <a href="#" class="shopUnitMore">
-                            Подробнее
-                        </a>
-                    </div>
-    
-                    <div class="shopUnit">
-                        <img src="http://placehold.it/308x231" />
-    
-                        <div class="shopUnitName">
-                            Название продукта
-                        </div>
-                        <div class="shopUnitShortDesc">
-                            Здесь будет немного текста описывающего продукт.
-                            Здесь будет немного текста описывающего продукт.
-                            Здесь будет немного текста описывающего продукт.
-                        </div>
-                        <div class="shopUnitPrice">
-                            Цена: 0000 $
-                        </div>
-                        <a href="#" class="shopUnitMore">
-                            Подробнее
-                        </a>
-                    </div>
-    
-                    <div class="shopUnit">
-                        <img src="http://placehold.it/308x231" />
-    
-                        <div class="shopUnitName">
-                            Название продукта
-                        </div>
-                        <div class="shopUnitShortDesc">
-                            Здесь будет немного текста описывающего продукт.
-                            Здесь будет немного текста описывающего продукт.
-                            Здесь будет немного текста описывающего продукт.
-                        </div>
-                        <div class="shopUnitPrice">
-                            Цена: 0000 $
-                        </div>
-                        <a href="#" class="shopUnitMore">
-                            Подробнее
-                        </a>
-                    </div>
-                </div>
-            </div>
-    
-            <div style="margin-bottom: 20px; float: left;">
-                <hr>
-                <h2>
-                    Это будет отдельная страница с открытым товаром
-                </h2>
-    
-                <div id="openedProduct-img">
-                    <img src="http://placehold.it/342x260">
-                </div>
-                <div id="openedProduct-content">
-                    <h1 id="openedProduct-name">
-                        Название продукта
-                    </h1>
-                    <div id="openedProduct-desc">
-                        Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
-                        Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
-                        Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
-                    </div>
-                    <div id="openedProduct-price">
-                        Цена: 0000 $
-                    </div>
-                </div>
-            </div>
-    
-    
-        </div>
-        
-    </div>
+            if(!isset($page)){
+                require('templates/main.php');
+            } elseif($page == 'shop'){
+                require('templates/shop.php');
+            } elseif($page == 'product'){
 
-    <footer>
-        <div id="footerInside">
-    
-            <div id="contacts">
-                <div class="contactWrap">
-                    <img src="img/icon-email.svg" class="contactIcon">
-                    info@YourShop.ru
-                </div>
-                <div class="contactWrap">
-                    <img src="img/phone-call.svg" class="contactIcon">
-                    8 800 *** ** **
-                </div>
-                <div class="contactWrap">
-                    <img src="img/placeholder.svg" class="contactIcon">
-                   Краснодар, ул. ??????, д. * офис ?????
-                </div>
-            </div>
-        
-            <div id="footerNav">
-                <a href="#">Главная</a>
-                <a href="#">Магазин</a>
-            </div>
-    
-            <div id="social">
-                <img class="socialItem" src="img/vk-social-logotype.svg">
-                <img class="socialItem" src="img/google-plus-social-logotype.svg">
-                <img class="socialItem" src="img/facebook-logo.svg">
-            </div>
-    
-            <div id="copyrights">&copy; YourShop</div>
+            }             
+            ?>
         </div>
-    </footer>
-</body>
+        <footer>
+            <div id="footerInside">
+        
+                <div id="contacts">
+                    <div class="contactWrap">
+                        <img src="img/icon-email.svg" class="contactIcon">
+                        info@YourShop.ru
+                    </div>
+                    <div class="contactWrap">
+                        <img src="img/phone-call.svg" class="contactIcon">
+                        8 800 *** ** **
+                    </div>
+                    <div class="contactWrap">
+                        <img src="img/placeholder.svg" class="contactIcon">
+                    Краснодар, ул. ??????, д. * офис ?????
+                    </div>
+                </div>
+            
+                <div id="footerNav">
+                <a href="/">Главная</a>
+                <a href="index.php?page=shop">Магазин</a>
+                </div>
+        
+                <div id="social">
+                    <img class="socialItem" src="img/vk-social-logotype.svg">
+                    <img class="socialItem" src="img/google-plus-social-logotype.svg">
+                    <img class="socialItem" src="img/facebook-logo.svg">
+                </div>
+        
+                <div id="copyrights">&copy; YourShop</div>
+            </div>
+        </footer>
+    </body>
 
 </html>
 
